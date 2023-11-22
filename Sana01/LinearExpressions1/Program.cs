4f9a9b0d8b8d5af6103c1297391514a2dbc77f46
ipz231_soyu <ipz231_soyu@student.ztu.edu.ua>
2023-11-22 10:38:05 +0200

    Fix errors in LinearExpressions1

--- Sana01/LinearExpressions1/Program.cs
+++ Sana01/LinearExpressions1/Program.cs
@@ -18,7 +18,7 @@ class Program
 
         double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (Math.Pow(a, 2)) / (Math.Pow(b, 2));
         double y = (5 * (a + b) * (c - d)) / ((1.0) / (2) * c) + Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a);
-        double z = (((Math.Pow(x, 2) - 2 * x) / (x * x)) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+        double z = (Math.Pow(Math.Pow(x, 2) - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
         double r = ((1.0) / (2) * a + (3.0) / (4) * b - (7.0) / (5)) / (3 * c + 1) + (1) / (a - c);
 
         Console.WriteLine("Значення x: " + x);
